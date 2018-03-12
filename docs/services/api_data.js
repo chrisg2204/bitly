8f83e9f026c0905412d1d754c0f29ab14d518b37
Christian Giménez <christiang15@hotmail.com>
2018-03-12 09:39:23 -0400

    Se agrego lo siguiente:
    documentación de servicios y clases.

---
+++ docs/services/api_data.js
@@ -0,0 +1,214 @@
+define({ "api": [
+  {
+    "type": "POST",
+    "url": "/shorten",
+    "title": "",
+    "version": "1.0.0",
+    "name": "addLink",
+    "group": "Link",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "url",
+            "description": "<p>Url que se va a recortar.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "data",
+            "description": "<p>Datos del url agregado.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Integer",
+            "optional": false,
+            "field": "code",
+            "description": "<p>Código HTTP.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "allowedValues": [
+              "null"
+            ],
+            "optional": false,
+            "field": "error",
+            "description": "<p>Objeto de errores.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n\t\"code\": 200,\n\t\"data\": \"Su url recortada http://127.0.0.1:9020/Z21haWwuY29t\",\n\t\"error\": false\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "./routes.js",
+    "groupTitle": "Link",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "allowedValues": [
+              "\"application/json\""
+            ],
+            "optional": false,
+            "field": "Content-Type",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "type": "Integer",
+            "optional": false,
+            "field": "code",
+            "description": "<p>Código HTTP.</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "type": "Object",
+            "allowedValues": [
+              "null"
+            ],
+            "optional": false,
+            "field": "data",
+            "description": "<p>Data de respuesta.</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "type": "Object",
+            "optional": false,
+            "field": "error",
+            "description": "<p>objeto de errores.</p>"
+          }
+        ]
+      }
+    }
+  },
+  {
+    "type": "GET",
+    "url": "/:hash",
+    "title": "",
+    "version": "1.0.0",
+    "name": "findOneLink",
+    "group": "Link",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "hash",
+            "description": "<p>Url encodificada (pathParam).</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "data",
+            "description": "<p>Redirecciona a la url original.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Integer",
+            "optional": false,
+            "field": "code",
+            "description": "<p>Código HTTP.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "allowedValues": [
+              "null"
+            ],
+            "optional": false,
+            "field": "error",
+            "description": "<p>Objeto de errores.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n\t\"code\": 200,\n\t\"data\": \"redireccionando..\",\n\t\"error\": false\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "./routes.js",
+    "groupTitle": "Link",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "allowedValues": [
+              "\"application/json\""
+            ],
+            "optional": false,
+            "field": "Content-Type",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "type": "Integer",
+            "optional": false,
+            "field": "code",
+            "description": "<p>Código HTTP.</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "type": "Object",
+            "allowedValues": [
+              "null"
+            ],
+            "optional": false,
+            "field": "data",
+            "description": "<p>Data de respuesta.</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "type": "Object",
+            "optional": false,
+            "field": "error",
+            "description": "<p>objeto de errores.</p>"
+          }
+        ]
+      }
+    }
+  }
+] });
---
+++ docs/services/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "bitlyClone",
+  "version": "1.0.0",
+  "description": "Api bitlyClone",
+  "title": "bitlyClone - Api docs",
+  "url": "http://127.0.0.1:9020",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-03-12T13:38:13.401Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
